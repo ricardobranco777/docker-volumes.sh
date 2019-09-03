3fa079dce15d4fd66e3fe18afef572f4140dad8a
Ricardo Branco <rbranco@suse.de>
2019-09-03 13:54:04 +0200

    Add support for Podman

--- docker-volumes.sh
+++ docker-volumes.sh
@@ -3,7 +3,7 @@
 # The docker-export and docker-commit/docker-save commands do not save the container volumes.
 # Use this script to save and load the container volumes.
 #
-# v1.7 by Ricardo Branco
+# v1.8 by Ricardo Branco
 #
 # NOTES:
 #  + This script could have been written in Python or Go, but the tarfile module and the tar package
@@ -23,8 +23,11 @@ fi
 
 IMAGE="ubuntu:18.04"
 
+# Set DOCKER=podman if you want to use podman.io instead of docker
+DOCKER=${DOCKER:-"docker"}
+
 get_volumes () {
-	cat <(docker inspect --type container -f '{{range .Mounts}}{{printf "%v\x00" .Destination}}{{end}}' "$CONTAINER" | head -c -1) | sort -uz
+	cat <($DOCKER inspect --type container -f '{{range .Mounts}}{{printf "%v\x00" .Destination}}{{end}}' "$CONTAINER" | head -c -1) | sort -uz
 }
 
 save_volumes () {
@@ -36,7 +39,7 @@ save_volumes () {
 	# Create a void tar file to avoid mounting its directory as a volume
 	touch -- "$TAR_FILE"
 	tmp_dir=$(mktemp -du -p /)
-	get_volumes | docker run --rm -i --volumes-from "$CONTAINER" -e LC_ALL=C.UTF-8 -v "$TAR_FILE:/${tmp_dir}/${TAR_FILE##*/}" $IMAGE tar -c -a $v --null -T- -f "/${tmp_dir}/${TAR_FILE##*/}"
+	get_volumes | $DOCKER run --rm -i --volumes-from "$CONTAINER" -e LC_ALL=C.UTF-8 -v "$TAR_FILE:/${tmp_dir}/${TAR_FILE##*/}" $IMAGE tar -c -a $v --null -T- -f "/${tmp_dir}/${TAR_FILE##*/}"
 }
 
 load_volumes () {
@@ -45,7 +48,7 @@ load_volumes () {
 		exit 1
 	fi
 	tmp_dir=$(mktemp -du -p /)
-	docker run --rm --volumes-from "$CONTAINER" -e LC_ALL=C.UTF-8 -v "$TAR_FILE:/${tmp_dir}/${TAR_FILE##*/}":ro $IMAGE tar -xp $v -S -f "/${tmp_dir}/${TAR_FILE##*/}" -C / --overwrite
+	$DOCKER run --rm --volumes-from "$CONTAINER" -e LC_ALL=C.UTF-8 -v "$TAR_FILE:/${tmp_dir}/${TAR_FILE##*/}":ro $IMAGE tar -xp $v -S -f "/${tmp_dir}/${TAR_FILE##*/}" -C / --overwrite
 }
 
 CONTAINER="$1"
